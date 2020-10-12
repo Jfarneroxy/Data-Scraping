08b26035c5cae067e3c7d451f667a120cd4295e5
Jacob Farner <32879558+jfarnerGit@users.noreply.github.com>
2020-10-11 22:25:48 -0700

    Update lyric_scraper.py

--- lyric_scraper.py
+++ lyric_scraper.py
@@ -3,11 +3,18 @@ from bs4 import BeautifulSoup
 import os
 import re
 
-GENIUS_API_TOKEN='cC-xIjHvk_L0UaJyCqCgHHBOT8eAYad0Cv2GpaLlZRkfN0oh-B-tl_bIQPPJRLZe'
+#You must insert your own authentication token
+GENIUS_API_TOKEN='INSERT TOKEN'
 
 
-# Functio to request artist data from Genius API
+
 def request_artist(artist_name, page):
+    '''
+    This function requests artist data from Genius
+    
+    artist_name = artist to selected data on 
+    page = artist url info
+    '''
     base_url = 'https://api.genius.com'
     headers = {'Authorization': 'Bearer ' + GENIUS_API_TOKEN}
     search_url = base_url + '/search?per_page=10&page=' + str(page)
@@ -16,21 +23,30 @@ def request_artist(artist_name, page):
     return response
 
 
-#function for requesting song urls from the Genius API
+
 def request_song(artist_name, song_cap):
+    '''
+    This function requests specific song data
+    
+    artist_name = artists to request song information from
+    song_cap = limits the number of songs requested for artists with large discographies
+    
+    '''
+    
     page = 1
     songs = []
 
     while True:
         response = request_artist(artist_name, page)
         json = response.json()
-        # Collect up to song_cap song objects from artist
+       
+    # Limiting script to collect data on song_cap number of songs
         song_info = []
         for hit in json['response']['hits']:
             if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
                 song_info.append(hit)
 
-        # Collect song URL's from song objects
+        # Collecting URLs from song objects
         for song in song_info:
             if (len(songs) < song_cap):
                 url = song['result']['url']
@@ -41,33 +57,42 @@ def request_song(artist_name, song_cap):
         else:
             page += 1
 
-    print('Found {} songs by {}'.format(len(songs), artist_name))
+    print('Successfully scraped {} songs by {}'.format(len(songs), artist_name))
 
     return songs
 
 
-#scrapes lyrics through genius API given song url
+
 def scrape_song(url):
+    '''
+    This function requests lyrics through the API based on song URL
+    
+    url = individual song url
+    '''
     page = requests.get(url)
     html = BeautifulSoup(page.text, 'html.parser')
     lyrics = html.find('div', class_='lyrics').get_text()
-    #remove identifiers like chorus, verse, etc
+    #removing labels within lyric data (ex: chorus, verse etc...)
     lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
-    #remove empty lines
+    #removing empty lines
     lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])
     return lyrics
 
-#writes scrapes lyrics to a new file
+
 def write_lyrics(artist_name, song_count):
-    f = open('/Users/jacobfarner/PycharmProjects/SpotifyTest/' + artist_name.lower() + '.txt', 'wb')
+    '''
+    This function writes collected lyrics to a new text file
+    artist_name = user selected artist -> used in .txt creation for organization
+    song_count = limits number of songs written
+    '''
+    f = open('/path' + artist_name.lower() + '.txt', 'wb')
     urls = request_song(artist_name, song_count)
     for url in urls:
         lyrics = scrape_song(url)
         f.write(lyrics.encode("utf8"))
     f.close()
-    num_lines = sum(1 for line in open('/Users/jacobfarner/PycharmProjects/SpotifyTest/' + artist_name.lower() + '.txt', 'rb'))
-    print('Wrote {} lines to file from {} songs'.format(num_lines, song_count))
-
+    num_lines = sum(1 for line in open('/path' + artist_name.lower() + '.txt', 'rb'))
+    print('Successfully wrote {} lines to new text file based on {} songs'.format(num_lines, song_count))
 
 
 write_lyrics('Drake', 100)
